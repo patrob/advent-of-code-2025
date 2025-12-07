0d308cd3f8f38d2b2bc295fd2500237c9d17d8c3
Patrick Robinson <patrob@gmail.com>
2025-12-06 23:37:17 -0600

    solved day 6 part 1

--- src/day06/index.test.ts
+++ src/day06/index.test.ts
@@ -1,18 +1,21 @@
 import { describe, it, expect } from 'vitest';
 import { part1, part2 } from './index.js';
 
-const exampleInput = ``;
+const exampleInput = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `;
 
 describe('Day 6', () => {
   describe('Part 1', () => {
     it('should solve example input', () => {
-      expect(part1(exampleInput)).toBe(0);
+      expect(part1(exampleInput)).toBe(4277556);
     });
   });
 
   describe('Part 2', () => {
     it('should solve example input', () => {
-      expect(part2(exampleInput)).toBe(0);
+      expect(part2(exampleInput)).toBe(3263827);
     });
   });
 });
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -3,9 +3,30 @@ import type { Solution } from '../types.js';
 
 export const part1: Solution = (input: string): number | string => {
   const lines = parseLines(input);
-  console.log(`Processing ${lines.length} lines...`);
+  const numbers = lines.slice(0, lines.length - 1).map(line => line.match(/[0-9]+/g)!.map(String).map(Number));
+  const correctedNumbers: number[][] = [];
+  for (let i = 0; i < numbers[0].length; i++) {
+    const newArray = [];
+    for (let j = 0; j < numbers.length; j++) {
+      newArray.push(numbers[j][i]);
+    }
+    correctedNumbers.push(newArray);
+  }
+  const operations = lines[lines.length -1].match(/\S/g)!.map(String);
 
-  return 0;
+  return operations.reduce((acc, current, index) => {
+    let mathOp: (a: number, b: number) => number;
+    let innerStart: number;
+    if (current === "+") {
+      mathOp = (a, b) => a + b;
+      innerStart = 0;
+    } else {
+      mathOp = (a, b) => a * b;
+      innerStart = 1;
+    }
+
+    return acc + correctedNumbers[index].reduce((innerAcc, innerCurrent) => mathOp(innerAcc, innerCurrent), innerStart);
+  }, 0);
 };
 
 export const part2: Solution = (input: string): number | string => {
